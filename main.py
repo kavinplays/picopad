0afb099b0e33f84562121e4ba03f003ee63c1280
kavinplays <59177133+kavinplays@users.noreply.github.com>
2021-03-07 00:20:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ import analogio
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
+#imported all the libraries we are going to use
 
-keyboard = Keyboard(usb_hid.devices)
+keyboard = Keyboard(usb_hid.devices) #Connected to the pc as a keyboard. Now the pc detects that a keyboard is attached.
 
-btn1 = digitalio.DigitalInOut(board.GP22)
-btn1.direction = digitalio.Direction.INPUT
-btn1.pull = Pull.DOWN
-btn2 = digitalio.DigitalInOut(board.GP21)
+btn1 = digitalio.DigitalInOut(board.GP22) #btn1 is initialized, i.e. it has given a variable name and told the pico where to find the button via GPIO(22 in this case)
+btn1.direction = digitalio.Direction.INPUT #it is stating that the button is input only which helps the pico understand that it is a button or switch that gives one of two values - 0 and 1
+btn1.pull = Pull.DOWN #the button is pulled down via code so that it doesn't debounce, i.e. act like it is pressed multiple times when it is actually pressed only ones
+btn2 = digitalio.DigitalInOut(board.GP21) #and the same is done for the rest of the buttons
 btn2.direction = digitalio.Direction.INPUT
 btn2.pull = Pull.DOWN
 btn3 = digitalio.DigitalInOut(board.GP20)
@@ -37,8 +38,8 @@ btn8.direction = digitalio.Direction.INPUT
 btn8.pull = Pull.DOWN
 
 
-button1 = 1 << 0
-button2 = 1 << 1
+button1 = 1 << 0 #the buttons are configured for the gamepad
+button2 = 1 << 1 #0 states that the first input in the gamepad is the first button and so on
 button3 = 1 << 2
 button4 = 1 << 3
 button5 = 1 << 4
@@ -46,8 +47,8 @@ button6 = 1 << 5
 button7 = 1 << 6
 button8 = 1 << 7
 
-pad = gamepad.GamePad(
-    btn1,
+pad = gamepad.GamePad( #Gamepad is initialized
+    btn1, # buttons are given in a order here you can swap button variable names if you connect say the third button to the pin of the fourth one
     btn2,
     btn3,
     btn4,
@@ -56,18 +57,18 @@ pad = gamepad.GamePad(
     btn7,
     btn8,
 )
-adc1 = analogio.AnalogIn(board.GP27)
-adc2 = analogio.AnalogIn(board.GP26)
-led1 = digitalio.DigitalInOut(board.GP13)
-led1.direction = digitalio.Direction.OUTPUT
-led1.value = True
+adc1 = analogio.AnalogIn(board.GP27) #one of the two potentiometers is initialized and is pointed to GP27
+adc2 = analogio.AnalogIn(board.GP26) #PS potentiometers and other analogio only works on the pins GP26, GP27 and GP28
+led1 = digitalio.DigitalInOut(board.GP13) #the power led is initialized and is pointed to GP12
+led1.direction = digitalio.Direction.OUTPUT #it is stated that the led is output device for the pico
+led1.value = True #the led is turned on, this is a power led it indicates that the code on pico is running
 
 led2 = digitalio.DigitalInOut(board.GP11)
 led2.direction = digitalio.Direction.OUTPUT
-led2.value = False
+led2.value = False #indicator led is turned off as if a program is stopped when it was on, then it wouldn't turn off until a button is pressed
 
-last_monotonic_time = -1
-sleep_monotonic_time = 0.01
+last_monotonic_time = -1 #this is supposed to be the value used to store last time
+sleep_monotonic_time = 0.01 #this is the value between when the keys are pressed and released. it is 10 miliseconds (0.01 seconds)
 
 keyboard.press(Keycode.F19, Keycode.Z)
 now_start = time.monotonic()
